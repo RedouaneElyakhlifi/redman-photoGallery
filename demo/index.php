912a4b5d688c0a298ed74bf36e4244cd93c2920a
Redouane El-Yakhlifi <49393760+RedouaneElyakhlifi@users.noreply.github.com>
2019-11-13 12:29:10 +0100

    Add files via upload

--- demo/index.php
+++ demo/index.php
@@ -11,13 +11,34 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
-    <link rel="stylesheet" href="css/redman-photoGallery.css">
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/redman-photoGallery.min.css">
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+
     
+    <style>
+    li{
+    list-style-type:none !important;
+}
+#masthead{
+    background-image:url(img/masthead.jpg);
+    background-repeat: no-repeat;
+    background-size:cover;
+    background-position:center;
+}
+#overlay{
+    background-color:rgb(0,0,0,0.8);
+}
+.text-custom{
+    text-shadow: black 5px 5px 1px;
+}
+.text-orange{
+    color:rgb(255, 123, 0);
+}
+.bg-lightgrey{
+    background-color:rgb(240, 240, 240);
+}</style>
 </head>
 <body class="p-0 m-0">
     <!--title-->
@@ -52,11 +73,21 @@
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock text-danger m-2"></i>redman-photoGallery.css <br><br><a target="_blank" class="text-danger" href='https://github.com/RedouaneElyakhlifi/redman-photoGallery'>download FREE on GIThub</a></li>
                                 <li class="m-0 m-md-2 mt-md-5 ml-md-5 pt-3"><i class="icon-wrench m-md-2 mt-2"></i>How to link the dependencies to your website:
                                     <ul class="p-0 p-md-3 font-weight-normal mt-2 ml-md-5">
-                                        <li class="m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>3th party libraries can be linked in the header together with all css! Include the redman-photoGallery.css as last added css!</li>
+                                        <li class="m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Link the CSS at the top of the page in the header!</li>
                                         <li class="m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Name redman-photoGallery.css with the path that YOU HAVE SAVED IT ON!</li>
                                         <li class=' mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/linking-header.jpg" alt="fout"></li>
-                                        <li class=" m-0 m-md-2 pt-2"><i class="icon-lock h6 text-danger m-2"></i>EXCEPTION: redman-photoGallery.js needs to be inserted at the end of your webpage!</li>
+                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong></li><br>
+                                        <li class='mt-2 bg-lightgrey '>&#x3c;link rel="stylesheet" href="https://<br class="d-md-none">stackpath.<br class="d-md-none">bootstrapcdn.<br class="d-md-none">com<br class="d-md-none">/bootstrap/<br class="d-md-none">4.3.1/css/bootstrap.<br class="d-md-none">min.css"><br>
+                                        &#x3c;link rel="stylesheet" href="https://<br class="d-md-none">cdnjs.<br class="d-md-none">cloudflare.com/<br class="d-md-none">ajax/libs/simple-line-icons<br class="d-md-none">/2.4.1/css/simple-line-icons.css"><br>
+                                        &#x3c;link rel="stylesheet" href="css/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.css"></li><br>
+                                        <li class=" m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Javasript dependecies need to be inserted just before the body end tag!</li>
+                                        <li class="m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Name redman-photoGallery.js with the path that YOU HAVE SAVED IT ON!</li>
                                         <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/linking-footer.jpg" alt="fout"></li>
+                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong></li><br>
+                                        <li class='mt-2 bg-lightgrey '>&#x3c;script src="https://<br class="d-md-none">code.<br class="d-md-none">jquery.com/<br class="d-md-none">jquery-3.3.1.<br class="d-md-none">min.js">&#x3c;/script><br>
+                                        &#x3c;script src="https://<br class="d-md-none">code.<br class="d-md-none">jquery.com/ui/1.12.1/<br class="d-md-none">jquery-ui.min.js">&#x3c;/script><br>
+                                        &#x3c;script src="https://<br class="d-md-none">stackpath<br class="d-md-none">.bootstrapcdn<br class="d-md-none">.com/<br class="d-md-none">bootstrap/<br class="d-md-none">4.3.1/js/<br class="d-md-none">bootstrap<br class="d-md-none">.min.js">&#x3c;/script><br>
+                                        &#x3c;script src="js/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.js">&#x3c;/script></li><br>
                                     </ul>
                                 </li>
                             </ul>
@@ -66,6 +97,7 @@
             </div>
         </div>
     </div>
+    
     <!--plugin global settings-->
         <div class="container-fluid pb-5 bg-light border-top">
             <div class='row no-gutters m-auto'>
@@ -76,9 +108,10 @@
                             <ul class="pt-2 h4 font-weight-bold">
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock text-danger m-md-2"></i>class="<span class="text-danger">redman<span class="d-inline-block d-md-none">&nbsp</span>-gallery</span>" -->this cannot change!! Use this class to adress the plugin.</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-totalCount="<span class="text-primary"> ENTER HERE the number of total images you have in the gallery.</span>"</li>
-                                <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-countPerPage="<span class="text-primary"> ENTER HERE the number of images per page that you want to show.</span>"if less then total images it activates pagination.</li>
+                                <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-countPerPage="<span class="text-primary"> ENTER HERE the number of images per page that you want to show.</span>" if less then total images it activates pagination.</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-countPerRow="<span class="text-primary"> ENTER HERE the number of images you want to place per row.</span>"</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-startHeight="<span class="text-primary"> ENTER HERE the height of the images.</span>"</li>
+                                <li class="m-0 m-md-2 pt-3"><i class="icon-lock  text-danger m-md-2"></i>data-redman-screenwidth="<span class="text-primary"> ENTER HERE the width of your screen while developing</span>" this concerns responsiveness</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-options text-dark m-md-2"></i>data-redman-theme="<span class="text-primary"> ENTER HERE dark to change ur pagination buttons to be visible on dark backgrounds.</span>"</li>
                                 <li class="m-0 m-md-2 mt-md-5 ml-md-5 pt-3"><i class="icon-wrench m-md-2 mt-2"></i>code excample(this is just an excample, you can use your own variables depending on your needs!): 
                                     <ul class="p-0 p-md-3 font-weight-normal mt-2 ml-md-5">
@@ -87,12 +120,13 @@
                                         <li class="pt-2"><i class="icon-lock-open h6 text-success m-md-2"></i>data-redman-countPerPage = 6 (images per page).</li>
                                         <li class="pt-2"><i class="icon-lock-open h6 text-success m-md-2"></i>data-redman-countPerRow = 3 (images per row).</li>
                                         <li class="pt-2"><i class="icon-lock-open h6 text-success m-md-2"></i>data-redman-startHeight = 250 (images height = 250px).</li>
+                                        <li class="pt-2"><i class="icon-lock h6 text-danger m-md-2"></i>data-redman-screenwidth = 1920 (my device's screenwidth while developing).</li>
                                         <li class="pt-2"><i class="icon-options h6 text-dark m-md-2"></i>data-redman-theme = dark  (breadcrumbs and pagination-button colors change).</li>
                                         <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/data-attr.jpg" alt="fout"></li>
                                         <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i>Put all of your images with the correct path inside this div.</li>
                                         <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/putting-img.jpg" alt="fout"></li><br>
                                         <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty DIV :</strong></li><br>
-                                        <li class='mt-2 bg-lightgrey '>&#x3c;div class="redman-gallery" data-redman-totalCount="" data-redman-countPerPage="" data-redman-countPerRow="" data-redman-startHeight="" data-redman-theme=""&#x3e; &#x3c;div&#x3e;</li><br>
+                                        <li class='mt-2 bg-lightgrey '>&#x3c;div class="redman-gallery" data-redman-totalCount="" data-redman-countPerPage="" data-redman-countPerRow="" data-redman-startHeight="" data-redman-theme="" data-redman-screenwidth=""&#x3e; &#x3c;/div&#x3e;</li><br>
                                         <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty IMG :</strong></li><br>
                                         <li class='mt-2 bg-lightgrey '>&#x3c;img src="" alt=""&#x3e;</li>
                                     </ul>
@@ -111,7 +145,7 @@
             </div>
         </div>
     <!--first excample-->
-    <div class="container-fluid bg-dark text-white pt-5 pb-5">
+    <div class="container-fluid bg-dark text-white p-0 m-0 pt-5 pb-5">
         <div class="container bg-dark text-white">
             <h2 class="m-2 text-center"><u><strong>Dark Theme</strong></u></h2>
             <div class="row no-gutters m-auto pt-5">
@@ -119,14 +153,14 @@
                 <div class="col-12 order-2 order-md-1 col-md-6 pr-3 pt-5 pt-md-0">
 
 
-                    <div class="redman-gallery" data-redman-totalCount="17" data-redman-countPerPage="6" data-redman-countPerRow="3" data-redman-startHeight="250" data-redman-theme="dark">
+                    <div class="redman-gallery" data-redman-totalCount="17" data-redman-countPerPage="6" data-redman-countPerRow="3" data-redman-startHeight="200" data-redman-theme="dark" data-redman-screenwidth="1920">
                         <img src="img/test1.jpg" alt="wrong path">
                         <img src="img/test2.jpg" alt="wrong path">
                         <img src="img/test3.jpg" alt="wrong path">
                         <img src="img/test4.jpg" alt="wrong path">
                         <img src="img/test5.jpg" alt="wrong path">
                         <img src="img/test6.jpg" alt="wrong path">
-                        <img src="img/test7.jpg" alt="wrong path">
+                        <img src="img/masthead.jpg" alt="wrong path">
                         <img src="img/test8.jpg" alt="wrong path">
                         <img src="img/test9.jpg" alt="wrong path">
                         <img src="img/test10.jpg" alt="wrong path">
@@ -163,14 +197,14 @@
         </div>
     </div>
     <!--2nd excample-->
-    <div class="container-fluid bg-light text-dark pt-5 pb-5">
+    <div class="container-fluid bg-light text-dark p-0 m-0 pt-5 pb-5">
         <div class="container">
         <h2 class="m-2 text-center"><u><strong>Default Theme</strong></u></h2>
             <div class='row no-gutters m-auto'>
                 <!--2nde excample information-->
                 <div class="col-12 col-md-6 font-weight-bold border-bottom border-dark">
                     <h3 class=" mt-5 ml-3 text-center"><u><strong>Check Out the excample <span class="d-none d-md-inline">right!</span><span class=" d-inline d-md-none">below!</span></strong></u></h3>
-                    <p class="d-none d-md-block">Here you can see a setup with text on the right and the photogallery on the left.</p>
+                    <p class="d-none d-md-block">Here you can see a setup with text on the left and the photogallery on the right.</p>
                     <ul class="mt-5 h5">
                         <li class="m-2"><i class="icon-diamond h6 text-orange m-2"></i>WITHOUT PAGINATION!! (put settings: data-redman-totalCount and data-redman-countPerPage equal to each other)</li>
                         <li class="m-2"><i class="icon-diamond h6 text-orange m-2"></i>NEST the photogallery's ANYWHERE YOU WANT!</li>
@@ -187,7 +221,7 @@
                 </div>
                     <!--2nde excample gallery-->
                     <div class="col-12 col-md-6 mt-5">
-                        <div class="redman-gallery" data-redman-totalCount="9" data-redman-countPerPage="10" data-redman-countPerRow="3" data-redman-startHeight="250" data-redman-theme="light">
+                        <div class="redman-gallery" data-redman-totalCount="9" data-redman-countPerPage="9" data-redman-countPerRow="3" data-redman-startHeight="200" data-redman-theme="light" data-redman-screenwidth="1920">
                             <img src="img/test18.jpg" alt="fout">
                             <img src="img/test19.jpg" alt="fout">
                             <img src="img/test20.jpg" alt="fout">
@@ -197,12 +231,21 @@
                             <img src="img/test24.jpg" alt="fout">
                             <img src="img/test25.jpg" alt="fout">
                             <img src="img/test26.jpg" alt="fout">
-                            <img src="img/test27.jpg" alt="fout">
                         </div>
                     </div>
                 </div>
             </div>
 </div>
+<section class="container-fluid d-none d-md-block">
+<div class="row no-gutters ">
+<div class="col-12 ">
+<h3 class=" mt-5 ml-3"><u><strong>VIDEO TUTORIAL: </strong></u></h3>
+</div>
+<div class="col-12 pb-3 pt-3 d-flex justify-content-center">
+<iframe width="735" height="600" src="https://www.youtube.com/embed/DEQJp3fCzy0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+</div>
+</div>
+</section>
 <footer>
 <div id="masthead" class="m-0 p-0 text-custom text-orange">
             <div id="overlay" class="p-0 m-0">
@@ -210,7 +253,9 @@
             </div>
         </div>
 </footer>
-        <script src="js/redman-photoGallery.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="js/redman-photoGallery.min.js"></script>
     </body>
 </html>
