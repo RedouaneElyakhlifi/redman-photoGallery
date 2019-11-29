8a6a5a580902a2a6b84a4a77b233acec82ccc4db
Redouane El-Yakhlifi <49393760+RedouaneElyakhlifi@users.noreply.github.com>
2019-11-29 13:28:58 +0100

    Add files via upload
    HTML validator green checked

--- demo/index.php
+++ demo/index.php
@@ -11,19 +11,22 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
-    <link rel="stylesheet" href="css/redman-photoGallery.min.css">
+    <link rel="stylesheet" href="css/redman-photoGallery.css">
     
     <style>
     li{
     list-style-type:none !important;
 }
-#masthead{
+iframe{
+    border:0;
+}
+.masthead{
     background-image:url(img/masthead.jpg);
     background-repeat: no-repeat;
     background-size:cover;
     background-position:center;
 }
-#overlay{
+.overlay{
     opacity: 0.8;
     background-color:rgb(0,0,0);
 }
@@ -40,8 +43,8 @@
 <body class="p-0 m-0">
     <!--title-->
     <div class="container-fluid bg-light text-dark p-0">
-        <div id="masthead" class="m-0 p-0 text-custom text-orange">
-            <div id="overlay" class="pt-5 pb-5">
+        <div class=" m-0 p-0 text-custom text-orange masthead">
+            <div class="overlay pt-5 pb-5">
                 <h1 class="font-weight-bold text-center pt-4 pb-5  m-0"><u>Redman's Foto-Gallery PLUGIN</u></h1>
                 <h3 class="text-center font-weight-bold pb-4  m-0 text-light">Hi all !! Welcome to my FREE to use photoGallery-plugin.</h3>
                 <h3 class="text-center font-weight-bold pb-4  m-0 text-light">Enjoy!</h3>
@@ -73,18 +76,18 @@
                                         <li class="m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Link the CSS at the top of the page in the header!</li>
                                         <li class="m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Name redman-photoGallery.css with the path that YOU HAVE SAVED IT ON!</li>
                                         <li class=' mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/linking-header.jpg" alt="fout"></li>
-                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong></li><br>
+                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong><br></li>
                                         <li class='mt-2 bg-lightgrey '>&#x3c;link rel="stylesheet" href="https://<br class="d-md-none">stackpath.<br class="d-md-none">bootstrapcdn.<br class="d-md-none">com<br class="d-md-none">/bootstrap/<br class="d-md-none">4.3.1/css/bootstrap.<br class="d-md-none">min.css"><br>
                                         &#x3c;link rel="stylesheet" href="https://<br class="d-md-none">cdnjs.<br class="d-md-none">cloudflare.com/<br class="d-md-none">ajax/libs/simple-line-icons<br class="d-md-none">/2.4.1/css/simple-line-icons.css"><br>
-                                        &#x3c;link rel="stylesheet" href="css/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.css"></li><br>
+                                        &#x3c;link rel="stylesheet" href="css/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.css"><br></li>
                                         <li class=" m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Javasript dependecies need to be inserted just before the body end tag!</li>
                                         <li class="m-0 m-md-2 pt-2"><i class="icon-lock h6 text-success m-2"></i>Name redman-photoGallery.js with the path that YOU HAVE SAVED IT ON!</li>
                                         <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/linking-footer.jpg" alt="fout"></li>
-                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong></li><br>
+                                        <li class="mt-4"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE URL's:</strong><br></li>
                                         <li class='mt-2 bg-lightgrey '>&#x3c;script src="https://<br class="d-md-none">code.<br class="d-md-none">jquery.com/<br class="d-md-none">jquery-3.3.1.<br class="d-md-none">min.js">&#x3c;/script><br>
                                         &#x3c;script src="https://<br class="d-md-none">code.<br class="d-md-none">jquery.com/ui/1.12.1/<br class="d-md-none">jquery-ui.min.js">&#x3c;/script><br>
                                         &#x3c;script src="https://<br class="d-md-none">stackpath<br class="d-md-none">.bootstrapcdn<br class="d-md-none">.com/<br class="d-md-none">bootstrap/<br class="d-md-none">4.3.1/js/<br class="d-md-none">bootstrap<br class="d-md-none">.min.js">&#x3c;/script><br>
-                                        &#x3c;script src="js/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.js">&#x3c;/script></li><br>
+                                        &#x3c;script src="js/<br class="d-md-none">redman-photoGallery<br class="d-md-none">.min.js">&#x3c;/script><br></li>
                                     </ul>
                                 </li>
                             </ul>
@@ -103,7 +106,7 @@
                     <ul class="mt-5 m-0 ml-md-3 h3 font-weight-bold pt-2">
                         <li class="pt-2"><i class="icon-wrench"></i>Settings (attributes):
                             <ul class="pt-2 h4 font-weight-bold">
-                                <li class="m-0 m-md-2 pt-3"><i class="icon-lock text-danger m-md-2"></i>class="<span class="text-danger">redman<span class="d-inline-block d-md-none">&nbsp</span>-gallery</span>" -->this cannot change!! Use this class to adress the plugin.</li>
+                                <li class="m-0 m-md-2 pt-3"><i class="icon-lock text-danger m-md-2"></i>class="<span class="text-danger">redman<span class="d-inline-block d-md-none">&nbsp;</span>-gallery</span>" -->this cannot change!! Use this class to adress the plugin.</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-totalCount="<span class="text-primary"> ENTER HERE the number of total images you have in the gallery.</span>"</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-countPerPage="<span class="text-primary"> ENTER HERE the number of images per page that you want to show.</span>" if less then total images it activates pagination.</li>
                                 <li class="m-0 m-md-2 pt-3"><i class="icon-lock-open text-success m-md-2"></i>data-redman-countPerRow="<span class="text-primary"> ENTER HERE the number of images you want to place per row.</span>"</li>
@@ -121,10 +124,10 @@
                                         <li class="pt-2"><i class="icon-options h6 text-dark m-md-2"></i>data-redman-theme = dark  (breadcrumbs and pagination-button colors change).</li>
                                         <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/data-attr.jpg" alt="fout"></li>
                                         <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i>Put all of your images with the correct path inside this div.</li>
-                                        <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/putting-img.jpg" alt="fout"></li><br>
-                                        <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty DIV :</strong></li><br>
-                                        <li class='mt-2 bg-lightgrey '>&#x3c;div class="redman-gallery" data-redman-totalCount="" data-redman-countPerPage="" data-redman-countPerRow="" data-redman-startHeight="" data-redman-theme="" data-redman-screenwidth=""&#x3e; &#x3c;/div&#x3e;</li><br>
-                                        <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty IMG :</strong></li><br>
+                                        <li class='mt-2'><small class="text-secondary">image excample:</small><br><img class="img-fluid" src="img/putting-img.jpg" alt="fout"><br></li>
+                                        <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty DIV :</strong><br></li>
+                                        <li class='mt-2 bg-lightgrey '>&#x3c;div class="redman-gallery" data-redman-totalCount="" data-redman-countPerPage="" data-redman-countPerRow="" data-redman-startHeight="" data-redman-theme="" data-redman-screenwidth=""&#x3e; &#x3c;/div&#x3e;<br></li>
+                                        <li class="mt-2"><i class="icon-lock-open h6 text-success m-md-2"></i><strong>COPY/PASTE empty IMG :</strong><br></li>
                                         <li class='mt-2 bg-lightgrey '>&#x3c;img src="" alt=""&#x3e;</li>
                                     </ul>
                                 </li>
@@ -150,7 +153,7 @@
                 <div class="col-12 order-2 order-md-1 col-md-6 pr-3 pt-5 pt-md-0">
 
 
-                    <div class="redman-gallery" data-redman-totalCount="17" data-redman-countPerPage="6" data-redman-countPerRow="3" data-redman-startHeight="200" data-redman-theme="dark" data-redman-screenwidth="1920">
+                    <div class="redman-gallery" data-redman-totalCount="16" data-redman-countPerPage="6" data-redman-countPerRow="3" data-redman-startHeight="200" data-redman-theme="dark" data-redman-screenwidth="1920">
                         <img src="img/test1.jpg" alt="wrong path">
                         <img src="img/test2.jpg" alt="wrong path">
                         <img src="img/test3.jpg" alt="wrong path">
@@ -239,13 +242,13 @@
 <h3 class=" mt-5 ml-3"><u><strong>VIDEO TUTORIAL: </strong></u></h3>
 </div>
 <div class="col-12 pb-3 pt-3 d-flex justify-content-center">
-<iframe width="735" height="600" src="https://www.youtube.com/embed/DEQJp3fCzy0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+<iframe width="735" height="600" src="https://www.youtube.com/embed/DEQJp3fCzy0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
 </div>
 </div>
 </section>
 <footer>
-<div id="masthead" class="m-0 p-0 text-custom text-orange">
-            <div id="overlay" class="p-0 m-0">
+<div class="m-0 p-0 text-custom text-orange masthead ">
+            <div class="overlay p-0 m-0">
             <p class="pb-5 m-0">Made by <a href="https://www.mijnsollicitatie.be" class="text-white">Redouane El-Yakhlifi</a> | v1.0 | nov. 2019 <span class="float-right pr-2">Free to use!</span></p>
             </div>
         </div>
@@ -253,6 +256,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="js/redman-photoGallery.min.js"></script>
+    <script src="js/redman-photoGallery.js"></script>
     </body>
 </html>
