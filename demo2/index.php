cde77eb05457fb38ec9ade3ac959f91688c18810
Aldo <aldo.fieuw@gmail.com>
2019-11-29 12:19:19 +0100

    update readme, created demo2

---
+++ demo2/index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Redman's Foto-Gallery PLUGIN</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description"
+    content="Redman's photoGallery PLUGIN || easy to use photo-gallery, all formats of images + easy CSS & HTML implementation. Change variables, Dynamic foto gallery PLUGIN || dependencies:jQuery, jQueryUI, simple-line-icons, Bootstrap, documentation">
+  <meta name="author" content="Redouane El-Yakhlifi">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+  <link rel="stylesheet" href="assets/css/redman-photoGallery.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <style>
+  /* Sticky footer styles
+-------------------------------------------------- */
+  html {
+    position: relative;
+    min-height: 100%;
+  }
+
+  body {
+    margin-bottom: 60px;
+    /* Margin bottom by footer height */
+  }
+
+  .footer {
+    margin-top: 3em;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    /* Set the fixed height of the footer here */
+    line-height: 60px;
+    /* Vertically center the text there */
+    background-color: #f5f5f5;
+  }
+  </style>
+</head>
+
+<body>
+  <div class="container mt-5">
+    <h1 class="text-center mb-3">Photogallery</h1>
+    <div class="row p-3 bg-dark text-white">
+      <div class="col-12 col-md-6 ">
+        <h2>Dark Theme</h2>
+        <h3>Pagination example!</h3>
+        <li>
+          <ul>
+            WITH PAGINATION!!</ul>
+          <ul>NEST the photogallery's ANYWHERE YOU WANT!</ul>
+          <ul>Clicking on the image enlarges it with a simple enlarged gallery mode:
+            <li>
+              <ul>Including all the images of the gallery and keeps count.</ul>
+              <ul> Includes next picture feature.</ul>
+              <ul>Includes previous picture feature.</ul>
+              <ul>Includes close button to return to website.</ul>
+            </li>
+          </ul>
+        </li>
+        Go ahead click on an image and enter the enlarged mode OR browse through the breadcrumbs and see the paginations
+        at work!
+      </div>
+      <div class="col-12 order-2 order-md-1 col-md-6 pr-3 pt-5 pt-md-0">
+        <div class="redman-gallery" data-redman-totalCount="17" data-redman-countPerPage="6" data-redman-countPerRow="3"
+          data-redman-startHeight="200" data-redman-theme="dark" data-redman-screenwidth="1519">
+          <img src="https://placeimg.com/434/500/any" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/animals" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/nature" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/people" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/tech" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch/grayscale" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/any" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/animals" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/nature" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/people" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/tech" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch/grayscale" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/people" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/tech" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch/grayscale" alt="this is an random image">
+        </div>
+      </div>
+    </div>
+    <div class="row p-2 pt-5 bg-light text-dark ">
+      <div class="col-12 col-md-6 ">
+        <h3>Pagination example!</h3>
+        <p>Here you can see a setup with text on the left and the photogallery on the right.</p>
+        <li>
+          <ul>
+            WITHOUT PAGINATION!! (put settings: data-redman-totalCount and data-redman-countPerPage equal to each other)
+          </ul>
+          <ul>NEST the photogallery's ANYWHERE YOU WANT!</ul>
+          <ul>Clicking on the image enlarges it with a simple enlarged gallery mode:
+            <li>
+              <ul>Including all the images of the gallery and keeps count.</ul>
+              <ul>Includes next picture feature.</ul>
+              <ul>Includes previous picture feature.</ul>
+              <ul>Includes close button to return to website.</ul>
+            </li>
+          </ul>
+        </li>
+        Go ahead click on an image and enter the enlarged mode!
+      </div>
+      <div class="col-12 col-md-6 mt-5">
+        <div class="redman-gallery" data-redman-totalCount="9" data-redman-countPerPage="9" data-redman-countPerRow="3"
+          data-redman-startHeight="200" data-redman-theme="light" data-redman-screenwidth="1920">
+          <img src="https://placeimg.com/434/500/any" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/animals" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/nature" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/people" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/tech" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/arch/grayscale" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/any" alt="this is an random image">
+          <img src="https://placeimg.com/434/500/animals" alt="this is an random image">
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="footer bg-dark text-white">
+    <div class="container ">
+      <div class="text-muted d-flex justify-content-between">
+        <div>
+          Made by <a href="https://www.mijnsollicitatie.be">Redouane
+            El-Yakhlifi</a> &amp; <a
+            href="https://github.com/RedouaneElyakhlifi/redman-photoGallery/graphs/contributors"
+            target="_blank">contributors</a></div>
+        <div>v1.0</div>
+        <div>nov. 2019 Free to use!</div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+  <script src="assets/js/redman-photoGallery.min.js"></script>
+
+</body>
+
+</html>
